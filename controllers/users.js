2efbf337c9cec8844f3d7ee2928efb52f74e2bc1
Aditya Pratap Singh <adityapratap141003@gmail.com>
2026-02-24 21:02:28 +0530

    Name change

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ module.exports.signup = async(req, res, next) => {
             if(err) {
                 return next(err);
             }
-            req.flash("success", "Welcome to WanderLust!");
+            req.flash("success", "Welcome to Voyage Vistas!");
             let redirectUrl = res.locals.redirectUrl || "/listings";
             delete req.session.redirectUrl;
             res.redirect(redirectUrl);
@@ -31,7 +31,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async(req, res) => {
-    req.flash("success", "Welcome back to WanderLust!");
+    req.flash("success", "Welcome back to Voyage Vistas!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     delete req.session.redirectUrl;
     res.redirect(redirectUrl);
